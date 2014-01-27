b12a6541fb42936da0e015d5489cc8d0e544cf9c
Makoto OOSUGI <oosugi20@gmail.com>
2014-01-28 03:50:44 +0900

    Fix bug

--- src/jquery.validator.js
+++ src/jquery.validator.js
@@ -263,7 +263,6 @@ Module = function (element, options) {
 	 * isSame
 	 */
 	fn.isSame = function ($input, $target) {
-		console.log($input.val(), $target.val());
 		return $input.val() === $target.val();
 	};
 
@@ -303,7 +302,7 @@ Module = function (element, options) {
 		this.$unit.each(function () {
 			var $this = $(this);
 			var $input = (_this.type === 'radio' || _this.type === 'checkbox') ? $this.find(_this.$input.filter(':checked')) : $this;
-			var $target = $('[data-validator="' + $this.attr('data-validator-reinputtarget') + '"');
+			var $target = $('[data-validator-id="' + $this.attr('data-validator-reinputtarget') + '"');
 
 			if (!_this.isSame($input, $target)) {
 				result = false;
